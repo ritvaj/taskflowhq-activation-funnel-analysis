b454eba5d0e3ce7ab90d699daeb31abcecfeb63d
Ritvaj Madotra <67282401+ritvaj@users.noreply.github.com>
2025-12-13 12:56:23 +0530

    Update 02_event_mapping_step2_onboarding.sql

--- sql/02_event_mapping_step2_onboarding.sql
+++ sql/02_event_mapping_step2_onboarding.sql
@@ -1,1 +1,13 @@
-
+-- Step 2: Onboarding Screen Viewed
+SELECT
+  user_pseudo_id,
+  event_timestamp
+FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20201101`,
+UNNEST(event_params) AS ep
+WHERE event_name = 'page_view'
+  AND ep.key = 'page_title'
+  AND ep.value.string_value IN (
+    'Home',
+    'Google Online Store',
+    'The Google Merchandise Store - Log In'
+  );
