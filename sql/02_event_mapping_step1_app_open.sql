6dfc073735e0395df3e95f4727b53014fa83d983
Ritvaj Madotra <67282401+ritvaj@users.noreply.github.com>
2025-12-13 15:37:33 +0530

    Update 02_event_mapping_step1_app_open.sql

--- sql/02_event_mapping_step1_app_open.sql
+++ sql/02_event_mapping_step1_app_open.sql
@@ -1,1 +1,8 @@
+We defined the funnel entry as unique users who triggered a session_start event, anchored to their first session timestamp per day.
 
+SELECT
+  user_pseudo_id,
+  MIN(event_timestamp) AS session_start_ts
+FROM events_20201101
+WHERE event_name = 'session_start'
+GROUP BY user_pseudo_id;
