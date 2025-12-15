c3bf6d6b89cd1addb0e383217d51a40970678ab0
Ritvaj Madotra <67282401+ritvaj@users.noreply.github.com>
2025-12-15 13:32:35 +0530

    Update 08_retention_cohort.sql

--- sql/08_retention_cohort.sql
+++ sql/08_retention_cohort.sql
@@ -12,5 +12,13 @@ WHERE
 
 
 -- Day 3 Retention
--- logic here
-
+CREATE OR REPLACE TABLE
+`psyched-hook-481007-m9.taskflowhq_analytics.day3_retention_users` AS
+SELECT DISTINCT
+  o.user_pseudo_id
+FROM `psyched-hook-481007-m9.taskflowhq_analytics.step2_onboarding_users` o
+JOIN `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*` e
+  ON o.user_pseudo_id = e.user_pseudo_id
+WHERE
+  PARSE_DATE('%Y%m%d', e.event_date)
+    = DATE(TIMESTAMP_MICROS(o.onboarding_ts)) + 3;
