7ac711e77f83a1d383869657e7ac8328bf358a43
Ritvaj Madotra <67282401+ritvaj@users.noreply.github.com>
2025-12-28 19:15:22 +0530

    Update 04_event_mapping_step3_feature_discovery.sql

--- sql/pipeline/04_event_mapping_step3_feature_discovery.sql
+++ sql/pipeline/04_event_mapping_step3_feature_discovery.sql
@@ -1,6 +1,6 @@
-Out of 603 onboarded users, 201 users proceeded to explore at least one feature.
-
 -- Step 3: Feature Discovery (user-level)
+
+-- This query captures the first `view_item` event per onboarded user, occurring after onboarding.
   
 CREATE OR REPLACE TABLE
 `psyched-hook-481007-m9.taskflowhq_analytics.step3_feature_discovery_users` AS
@@ -13,3 +13,5 @@ JOIN `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20201101` e
 WHERE e.event_name = 'view_item'
   AND e.event_timestamp > s.onboarding_ts
 GROUP BY s.user_pseudo_id;
+
+-- Out of 603 users who completed onboarding, 201 reached feature discovery.
