c7dda022cc29912328172714247e41e2b56f3af6
Ritvaj Madotra <67282401+ritvaj@users.noreply.github.com>
2025-12-29 16:30:27 +0530

    Update 02_retention_cohort.sql

--- sql/analysis/02_retention_cohort.sql
+++ sql/analysis/02_retention_cohort.sql
@@ -1,36 +1,32 @@
--- Day 1 Retention
+-- Retention: Onboarding Cohort (D1 / D3 / D7)
+-- Measures short-term retention starting from onboarding, the first moment of informed exposure.
+
+
 CREATE OR REPLACE TABLE
-`psyched-hook-481007-m9.taskflowhq_analytics.day1_retention_users` AS
+`psyched-hook-481007-m9.taskflowhq_analytics.onboarding_retention` AS
 SELECT DISTINCT
-  o.user_pseudo_id
+  o.user_pseudo_id,
+  DATE_DIFF(
+    PARSE_DATE('%Y%m%d', e.event_date),
+    DATE(TIMESTAMP_MICROS(o.onboarding_ts)),
+    DAY
+  ) AS day_offset
 FROM `psyched-hook-481007-m9.taskflowhq_analytics.step2_onboarding_users` o
 JOIN `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*` e
   ON o.user_pseudo_id = e.user_pseudo_id
-WHERE
-  PARSE_DATE('%Y%m%d', e.event_date)
-    = DATE(TIMESTAMP_MICROS(o.onboarding_ts)) + 1;
+WHERE DATE_DIFF(
+    PARSE_DATE('%Y%m%d', e.event_date),
+    DATE(TIMESTAMP_MICROS(o.onboarding_ts)),
+    DAY
+  ) IN (1, 3, 7);
 
+--
 
--- Day 3 Retention
-CREATE OR REPLACE TABLE
-`psyched-hook-481007-m9.taskflowhq_analytics.day3_retention_users` AS
-SELECT DISTINCT
-  o.user_pseudo_id
-FROM `psyched-hook-481007-m9.taskflowhq_analytics.step2_onboarding_users` o
-JOIN `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*` e
-  ON o.user_pseudo_id = e.user_pseudo_id
-WHERE
-  PARSE_DATE('%Y%m%d', e.event_date)
-    = DATE(TIMESTAMP_MICROS(o.onboarding_ts)) + 3;
+SELECT day_offset, COUNT(DISTINCT user_pseudo_id)
+FROM `psyched-hook-481007-m9.taskflowhq_analytics.onboarding_retention`
+GROUP BY day_offset
+ORDER BY day_offset;
 
--- Day 7 Retention
-CREATE OR REPLACE TABLE
-`psyched-hook-481007-m9.taskflowhq_analytics.day7_retention_users` AS
-SELECT DISTINCT
-  o.user_pseudo_id
-FROM `psyched-hook-481007-m9.taskflowhq_analytics.step2_onboarding_users` o
-JOIN `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*` e
-  ON o.user_pseudo_id = e.user_pseudo_id
-WHERE
-  PARSE_DATE('%Y%m%d', e.event_date)
-    = DATE(TIMESTAMP_MICROS(o.onboarding_ts)) + 7;
+-- Retention drops: Day 1 = 51
+-- Day 3 = 17
+-- Day 7 = 5
